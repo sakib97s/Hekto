080bd5d52abaadf2ea4200a5e2bd2685bc20fdd5
Sakib Hossain <sakibs.ngn@gmail.com>
2024-08-03 17:48:51 +0600

    reeight

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,14 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <>
-            <section className="bg-[#EEEFFB] py-10 px-[10px] lg:mx-0">
+            <section className="bg-[#EEEFFB] py-10 px-[10px] lg:px-0">
                 <Container>
                     <div className="flex flex-wrap">
                         <div className="lg:w-[30%]">
                             <img src={logo} alt="" />
                             <div className="lg:w-[377px] my-7">
                                 <input type="email" name="" placeholder="Enter Email Address" className="lg:w-[238px] border-none rounded-l-md placeholder:font-lato placeholder:font-normal placeholder:text-[16px] placeholder:text-[#8A8FB9] focus:ring-[#FB2E86]" id="" />
-                                <button className="font-lato font-medium text-[16px] text-[#fff] pt-[8px] pb-[8px] px-[40px] bg-[#FB2E86]  rounded-r-md">Sign Up</button>
+                                <button className="font-lato font-medium text-[16px] text-[#fff] py-[8px] px-[20px] lg:px-[40px] bg-[#FB2E86]  rounded-r-md">Sign Up</button>
                             </div>
                             <div className="">
                                 <p className="font-lato font-normal text-[16px] text-[#8A8FB9]">Contact Info</p>
@@ -58,10 +58,10 @@ const Footer = () => {
                     </div>
                 </Container>
             </section>
-            <section className="bg-[#E7E4F8] py-[17px]">
-                <Container className='flex flex-wrap lg:justify-around text-center items-center'>
+            <section className="bg-[#E7E4F8] py-[17px] px-[10px] lg:px-0">
+                <Container className='flex flex-wrap lg:justify-around justify-between text-center items-center'>
                     <p className="font-lato font-semibold text-[16px] text-[#9DA0AE]">©Webecy - All Rights Reserved</p>
-                    <div className="flex items-center lg:w-[8%] justify-between">
+                    <div className="flex items-center lg:w-[8%] w-[30%] justify-between">
                         <div className="p-1 bg-[#151875] rounded-full">
                             <FaFacebookF className="text-[#fff] w-[15px] h-[15px]" />
                         </div>
